5fff3dfcafc4977e73f33ab9d6c4a9262cc101ae
Joshua Proctor <joshuaeup@gmail.com>
2020-07-22 17:54:20 -0400

    added /#/ to href

--- src/components/topNav/topNav.js
+++ src/components/topNav/topNav.js
@@ -13,19 +13,19 @@ class topNav extends React.Component {
     render() {
         return (
             <div id="homeResponsiveTopNav" className="d-flex justify-content-center">
-                <a href="/" className="homeResponsiveTopNavHoverHome justify-content-center align-items-center topNavText">
+                <a href="/#/" className="homeResponsiveTopNavHoverHome justify-content-center align-items-center topNavText">
                     Home
                 </a>
-                <a href="/bio" className="homeResponsiveTopNavHoverProfile justify-content-center align-items-center topNavText">
+                <a href="/#/bio" className="homeResponsiveTopNavHoverProfile justify-content-center align-items-center topNavText">
                     Profile
                 </a>
-                <a href="/portfolio" className="homeResponsiveTopNavHoverProjects justify-content-center align-items-center topNavText">
+                <a href="/#/portfolio" className="homeResponsiveTopNavHoverProjects justify-content-center align-items-center topNavText">
                     Projects
                 </a>
-                <a href="/experience" className="homeResponsiveTopNavHoverSkills justify-content-center align-items-center topNavText">
+                <a href="/#/experience" className="homeResponsiveTopNavHoverSkills justify-content-center align-items-center topNavText">
                     Skills
                 </a>
-                <a href="/contact" className="homeResponsiveTopNavHoverContact justify-content-center align-items-center topNavText">
+                <a href="/#/contact" className="homeResponsiveTopNavHoverContact justify-content-center align-items-center topNavText">
                     Contact
                 </a>          
           </div>
