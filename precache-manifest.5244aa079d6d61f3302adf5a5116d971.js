3f630600464f77c85c45f2a2465c95333512b403
Joshua Proctor <joshuaeup@gmail.com>
2020-07-21 20:13:39 -0400

    Updates

---
+++ precache-manifest.5244aa079d6d61f3302adf5a5116d971.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c275bb509749a21f7205967a946e10d",
+    "url": "/my-port/index.html"
+  },
+  {
+    "revision": "ddba5a42863edda7dc8e",
+    "url": "/my-port/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "982f8db5562c50b0d968",
+    "url": "/my-port/static/js/2.c7663357.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-port/static/js/2.c7663357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddba5a42863edda7dc8e",
+    "url": "/my-port/static/js/main.ffa8f59e.chunk.js"
+  },
+  {
+    "revision": "ab6064670bb214b93c34",
+    "url": "/my-port/static/js/runtime-main.98ba209a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-port/static/media/logo.5d5d9eef.svg"
+  }
+]);
